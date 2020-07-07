6630034dc0aade134092685d3179d5b11b821e48
Pablo <molinari.pablo81@gmail.com>
2020-07-06 23:28:35 -0300

    Docs added!

---
+++ docs/precache-manifest.cfafe5dffa8ddc9a33abc83be1642a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df95a9c60b00a11514b9279c42aec13c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0364c6bedbdc476de1c6",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2c0adb474f2b2bda3f9b",
+    "url": "/static/js/2.688726de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.688726de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0364c6bedbdc476de1c6",
+    "url": "/static/js/main.c22177a6.chunk.js"
+  },
+  {
+    "revision": "caf821b007d82d3e9b1c",
+    "url": "/static/js/runtime-main.c1659c69.js"
+  }
+]);
